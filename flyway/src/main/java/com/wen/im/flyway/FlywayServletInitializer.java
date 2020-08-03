4d6916c828f181dca30b9913fb2394799089aa86
musk <muskcool@protonmail.com>
2020-08-03 15:39:51 +0800

    Flyway Example

---
+++ flyway/src/main/java/com/wen/im/flyway/FlywayServletInitializer.java
@@ -0,0 +1,18 @@
+package com.wen.im.flyway;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ * 
+ * @author wen
+ */
+public class FlywayServletInitializer extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(FlywayApplication.class);
+    }
+}
